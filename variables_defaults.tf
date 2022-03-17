ea3fd8e7d15829eafb35b2cc3e103b146f8e3fd5
Relk Li <YiJiun.Li.C@gmail.com>
2022-03-17 18:03:07 +0800

    update aws-vpc-cni to v1.10.2 (#49)
    Co-authored-by: Relk <relk@maicoin.com>

--- variables_defaults.tf
+++ variables_defaults.tf
@@ -22,11 +22,11 @@ locals {
     }
     vpc_cni = {
       repo = "quay.io/amis/amazon-k8s-cni"
-      tag  = "v1.9.1"
+      tag  = "v1.10.2"
     }
     vpc_cni_init = {
       repo = "quay.io/amis/amazon-k8s-cni-init"
-      tag  = "v1.9.1"
+      tag  = "v1.10.2"
     }
     calico_node = {
       repo = "quay.io/calico/node"
