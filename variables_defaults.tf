a87a4abff6bd61326c6441ad8badcafd63ee07ad
smalltown <smalltown20110306@gmail.com>
2022-05-08 07:46:08 +0800

    use customized aws vpc cni image to workaround flatcar iptables upgrade to nftables

--- variables_defaults.tf
+++ variables_defaults.tf
@@ -22,7 +22,7 @@ locals {
     }
     vpc_cni = {
       repo = "quay.io/amis/amazon-k8s-cni"
-      tag  = "v1.11.0"
+      tag  = "v1.11.0-nftables"
     }
     vpc_cni_init = {
       repo = "quay.io/amis/amazon-k8s-cni-init"
